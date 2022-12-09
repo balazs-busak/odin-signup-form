2869e0bc1d8cec3ffb594c3432d4ffb09d5afcbc
busjee <busakbalazs@gmail.com>
2022-12-09 23:33:59 +0000

    easy to read code

--- script.js
+++ script.js
@@ -1,25 +1,3 @@
-//Need to find why not working, id? class?
-
-// const passwordInput = document.getElementById('password');
-// const togglePasswordButton = document.getElementById('toggle-password');
-
-// togglePasswordButton.addEventListener('click', togglePassword);
-
-// function togglePassword() {
-//   if (passwordInput.type === 'password') {
-//     passwordInput.type = 'text';
-//     togglePasswordButton.textContent = 'Hide password';
-//     togglePasswordButton.setAttribute('aria-label',
-//       'Hide password.');
-//   } else {
-//     passwordInput.type = 'password';
-//     togglePasswordButton.textContent = 'Show password';
-//     togglePasswordButton.setAttribute('aria-label',
-//       'Show password as plain text. ' +
-//       'Warning: this will display your password on the screen.');
-//   }
-// }
-
 
 function onChange() {
   const password = document.querySelector('input[name=password]');
